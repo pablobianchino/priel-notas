ef406a9c162999be1b4c79e447e2cd353a01d4e0
pablobianchino <pablobianchino@gmail.com>
2026-07-06 18:32:45 -0300

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
 const CACHE_NAME = 'priel-notas-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
+  '/',
+  '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
